cbce4669dcd817399994fc44ea2eec81c49aef67
pingu52 <gudrbs9852@gmail.com>
2024-03-27 23:57:32 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    for(int i = 0; i <= n; i++) {
+        if(i % 2 != 0)
+        answer.emplace_back(i);
+    }
+    return answer;
+}
