a71df4e0f5580361af72ec42c80276d8f24d5640
pingu52 <gudrbs9852@gmail.com>
2024-03-28 00:02:32 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    answer = ceil((double)n / 7.0);
+
+    return answer;
+}
