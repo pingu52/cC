671083a953f65883a9877950b7d99a0c0fd2327c
pingu52 <gudrbs9852@gmail.com>
2024-03-28 20:39:11 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int slice, int n) {
+    int answer = 0;
+    while(!(answer * slice / n >= 1)) answer++;
+    return answer;
+}
