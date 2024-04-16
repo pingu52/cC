65ebb3de54dd929a05475d6020548e5453766657
pingu52 <gudrbs9852@gmail.com>
2024-04-16 23:56:09 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 0.28 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string, vector<vector<int>> queries) {
+    string answer = "";
+    
+    for(int i = 0; i < queries.size(); i++) {
+        reverse(my_string.begin() + queries[i][0], my_string.begin() + queries[i][1] + 1);
+    }
+    answer = my_string;
+    return answer;
+}
