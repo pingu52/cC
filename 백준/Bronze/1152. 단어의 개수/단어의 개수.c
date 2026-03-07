5a994db8425da196afc92d25a8809836a2f84330
pingu52 <gudrbs9852@gmail.com>
2026-03-07 19:35:26 +0900

    [Bronze II] Title: 단어의 개수, Time: 4 ms, Memory: 2088 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    static char str[1000002];
+    int count = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ' && str[i] != '\n' &&
+            (i == 0 || str[i - 1] == ' ')) {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+    return 0;
+}
