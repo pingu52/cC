ecbbc8157a71b3c7e922b867d49c66d728014146
pingu52 <gudrbs9852@gmail.com>
2026-03-09 21:12:57 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.c
@@ -0,0 +1,47 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char str[102];
+
+    while (1) {
+        fgets(str, sizeof(str), stdin);
+
+        if (strcmp(str, ".\n") == 0 || strcmp(str, ".") == 0) {
+            break;
+        }
+
+        char stack[102];
+        int top = -1;
+        bool balanced = true;
+
+        for (int i = 0; str[i] != '\0'; i++) {
+            char ch = str[i];
+
+            if (ch == '(' || ch == '[') {
+                stack[++top] = ch;
+            } else if (ch == ')') {
+                if (top == -1 || stack[top] != '(') {
+                    balanced = false;
+                    break;
+                }
+                top--;
+            } else if (ch == ']') {
+                if (top == -1 || stack[top] != '[') {
+                    balanced = false;
+                    break;
+                }
+                top--;
+            }
+        }
+
+        if (balanced && top == -1) {
+            printf("yes\n");
+        } else {
+            printf("no\n");
+        }
+    }
+
+    return 0;
+}
